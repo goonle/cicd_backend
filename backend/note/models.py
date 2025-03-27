306d15babff8d7ea06aaf7c1d6139f78625b9223
goonle <junhuh741@gmail.com>
2025-03-27 19:16:54 +1300

    change code

--- backend/note/models.py
+++ backend/note/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
