1b32ca0636add790abc5708ab4d6f23d9958692d
goonle <junhuh741@gmail.com>
2025-03-27 19:54:05 +1300

    [Test] add test code

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User  # Use this instead of authtoken.admin.User
 
-# Create your tests here.
+class RegisterViewTest(APITestCase):
+    def test_register_user(self):
+        url = reverse('register')  # Make sure you name your URL pattern as 'register'
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword123',
+            'email': 'test@example.com'
+        }
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testuser')
